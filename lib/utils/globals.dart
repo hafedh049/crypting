0181f5c2902b467d7f478f56f98e40898f93b716
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-30 21:46:42 +0100

    Modified  lib/utils/globals.dart

--- lib/utils/globals.dart
+++ lib/utils/globals.dart
@@ -5,6 +5,5 @@ const Color gray = Colors.grey;
 const Color white = Colors.white;
 
 final Map<int, List<String>> salts = <int, List<String>>{
-  0: <String>["MD5 (Message Digest Algorithm 5)", "", ""],
-  0: <String>["MD5 (Message Digest Algorithm 5)", "", ""],
+  0: Map<String,dynamic>{},
 };
