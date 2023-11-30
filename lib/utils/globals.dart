e94f91f9417a69fd63e9af3f32d1a5c7e061a2cc
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-30 21:52:25 +0100

    Modified  lib/utils/globals.dart

--- lib/utils/globals.dart
+++ lib/utils/globals.dart
@@ -21,7 +21,7 @@ final Map<int, Map<String, dynamic>> salts = <int, Map<String, dynamic>>{
     "value": "",
   },
   3: <String, dynamic>{
-    "algorithm": "SHA-2 (Secure Hash Algorithm 2)",
+    "algorithm": "SHA-3 (Secure Hash Algorithm 3)",
     "types": <String>["SHA-224", "SHA-256", "SHA-384", "SHA-512"],
     "value": "",
   },
