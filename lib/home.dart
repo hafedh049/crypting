5525ac0096333df39a818352bede52580b3691eb
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-30 11:54:33 +0100

    Modified  lib/home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,4 @@
+import 'package:anim_search_bar/anim_search_bar.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -15,7 +16,7 @@ class _HomeState extends State<Home> {
         padding: const EdgeInsets.all(24),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[],
+          children: <Widget>[AnimSearchBar(width: width, textController: textController, onSuffixTap: onSuffixTap, onSubmitted: onSubmitted)],
         ),
       ),
     );
