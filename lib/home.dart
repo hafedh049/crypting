239d311de887e26ec5f5b37e33e7cae7012679ca
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-30 16:18:25 +0100

    Modified  lib/home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -10,12 +10,7 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   final TextEditingController _inputController = TextEditingController();
-  final List<Map<int, List<String>>> _salts = List<Map<int, List<String>>>.generate(
-    10,
-    (int index) => <int, List<String>>{
-      index: <String>[],
-    },
-  );
+
   @override
   void dispose() {
     _inputController.dispose();
