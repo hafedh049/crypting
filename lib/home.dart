41c11647f708791753b0593eb8ad4926b2d43b65
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-30 12:09:08 +0100

    Modified  lib/home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -31,7 +31,7 @@ class _HomeState extends State<Home> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             SearchBar(
-              textController: _inputController,
+              controller: _inputController,
               onSuffixTap: ,
               onSubmitted: onSubmitted,
             ),
