5429047705fee5a7b4927538aece342036033849
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-30 21:41:12 +0100

    Modified  lib/home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -46,9 +46,9 @@ class _HomeState extends State<Home> {
                         children: <Widget>[
                           Text(salts[index]![0], style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500, color: mainColor)),
                           const SizedBox(height: 10),
-                          Text(salts[index]![0], style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500, color: mainColor)),
+                          Text(salts[index]![1], style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500, color: mainColor)),
                           const SizedBox(height: 10),
-                          Text(salts[index]![0], style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500, color: mainColor)),
+                          Text(salts[index]![2], style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500, color: mainColor)),
                           const SizedBox(height: 10),
                         ],
                       ),
