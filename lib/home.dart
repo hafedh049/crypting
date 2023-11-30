c31f366bb69bc97a10e7be772618ed1affee5447
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-30 22:40:00 +0100

    Modified  lib/home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -52,7 +52,9 @@ class _HomeState extends State<Home> {
                             if (salts[index]!["title"] == algorithms["types"].keys[0])
                               Flexible(child: Text(salts[index]![2], style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500, color: mainColor.withOpacity(.4))))
                             else
-                              for (MapEntry type in algorithms["types"]) ...<Widget>[],
+                              for (MapEntry type in algorithms["types"]) ...<Widget>[
+                                Text(" ${algorithms["algorithm"]}", style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: mainColor.withOpacity(.6))),
+                              ],
                           ],
                           const SizedBox(height: 10),
                         ],
