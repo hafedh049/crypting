18e25b120b084f69a3c58af64fa77c34c74475a1
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-30 23:35:45 +0100

    Modified  lib/home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -30,6 +30,7 @@ class _HomeState extends State<Home> {
               SearchBarAnimation(
                 enteredTextStyle: const TextStyle(color: teal),
                 hintText: "Type ...",
+                cursorColour: teal,
                 hintTextColour: teal,
                 searchBoxColour: gray,
                 buttonColour: white.withOpacity(.3),
