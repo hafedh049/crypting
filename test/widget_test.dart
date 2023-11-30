8e81d52572a9e0c6a1ed586cee6f638b0e1271a0
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-30 16:19:06 +0100

    Modified  test/widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,8 +6,6 @@ import 'package:crypting/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     await tester.pumpWidget(const Main());
-
-    // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
 
