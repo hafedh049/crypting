f01479d98bb872be2e2c438410db099e0c6c8fff
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-30 16:17:58 +0100

    Modified  lib/home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -43,6 +43,7 @@ class _HomeState extends State<Home> {
                       padding: const EdgeInsets.all(16),
                       margin: const EdgeInsets.all(16),
                       decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
                         color: mainColor.withOpacity(.3),
                         boxShadow: <BoxShadow>[BoxShadow(color: gray.withOpacity(.3), blurStyle: BlurStyle.outer, offset: const Offset(2, 2))],
                       ),
