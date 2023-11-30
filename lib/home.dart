7a6f99ea916afeec9af7ac00a77c2a98a9a3db92
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-30 11:55:03 +0100

    Modified  lib/home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -19,6 +19,7 @@ class _HomeState extends State<Home> {
           children: <Widget>[
             AnimSearchBar(width: width, textController: textController, onSuffixTap: onSuffixTap, onSubmitted: onSubmitted),
             const SizedBox(height: 20),
+            ListView.builder(itemBuilder: itemBuilder),
           ],
         ),
       ),
