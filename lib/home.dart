814e8d638368571dbc8cfd263ed0871727a6d46d
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-30 21:39:44 +0100

    Modified  lib/home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -44,7 +44,7 @@ class _HomeState extends State<Home> {
                       ),
                       child: Column(
                         children: <Widget>[
-                          Text(salts[index][0]),
+                          Text(salts[index]![0]),
                         ],
                       ),
                     );
