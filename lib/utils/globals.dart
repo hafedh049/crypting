5941dbc9481ced9410cfede91042bc8361b5b494
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-30 22:03:01 +0100

    Modified  lib/utils/globals.dart

--- lib/utils/globals.dart
+++ lib/utils/globals.dart
@@ -94,7 +94,7 @@ final Map<int, Map<String, dynamic>> salts = <int, Map<String, dynamic>>{
         "value": "",
       },
       <String, dynamic>{
-        "algorithm": "Blowfish",
+        "algorithm": "Camellia",
         "types": <String>[],
         "value": "",
       },
