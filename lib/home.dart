2b5e992b9418719b5d92bef09c191294b780bc09
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-30 21:42:06 +0100

    Modified  lib/home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -44,11 +44,11 @@ class _HomeState extends State<Home> {
                       ),
                       child: Column(
                         children: <Widget>[
-                          Text(salts[index]![0], style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500, color: mainColor)),
+                          Text(salts[index]![0], style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: mainColor)),
                           const SizedBox(height: 10),
-                          Text(salts[index]![1], style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500, color: mainColor)),
+                          Text(salts[index]![1], style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500, color: mainColor.withOpacity(.6))),
                           const SizedBox(height: 10),
-                          Text(salts[index]![2], style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500, color: mainColor)),
+                          Text(salts[index]![2], style: const TextStyle(fontSize: 12, fontWeight: FontWeight.w500)),
                           const SizedBox(height: 10),
                         ],
                       ),
