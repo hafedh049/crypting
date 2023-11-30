a59ad54189c67f99f72ae01283a7560b22edcb7d
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-30 21:36:02 +0100

    Modified  lib/utils/globals.dart

--- lib/utils/globals.dart
+++ lib/utils/globals.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 const Color mainColor = Colors.tealAccent;
 const Color gray = Colors.grey;
+const Color white = Colors.white;
 
 final List<Map<int, List<String>>> salts = List<Map<int, List<String>>>.generate(
   10,
