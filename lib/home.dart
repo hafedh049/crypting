2607277f9e98ccd345a8cc37b5017c0dc201ae1b
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-30 23:00:52 +0100

    Modified  lib/home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -46,6 +46,9 @@ class _HomeState extends State<Home> {
                         children: <Widget>[
                           Text(salts[index]!["title"], style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500, color: mainColor)),
                           const SizedBox(height: 10),
+                          for (Map<String, dynamic> algorithms in salts[index]!["algorithms"]) ...<Widget>[
+                            Text(" ${algorithms["algorithm"]}", style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: mainColor.withOpacity(.8))),
+                          ],
                         ],
                       ),
                     );
