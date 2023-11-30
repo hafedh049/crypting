ca0f9ac52a743d86bd9f254820a442c76d23f5b5
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-30 21:36:37 +0100

    Modified  lib/utils/globals.dart

--- lib/utils/globals.dart
+++ lib/utils/globals.dart
@@ -4,7 +4,4 @@ const Color mainColor = Colors.tealAccent;
 const Color gray = Colors.grey;
 const Color white = Colors.white;
 
-final List<Map<int, List<String>>> salts = List<Map<int, List<String>>>.generate(
-  10,
-  (int index) => <int, List<String>>{index: <String>[]},
-);
+final List<Map<int, List<String>>> salts = List<Map<int, List<String>>>.generate(10, (int index) => <int, List<String>>{index: <String>[]});
